b7195875e23baa68d887b9ddf6dd95fe9d2a08db
Mintheprudent <kyh6102min@gmail.com>
2025-08-15 20:51:18 +0900

    Update main.py

--- Day02/main.py
+++ Day02/main.py
@@ -1,19 +1,19 @@
-# Day 2: 사용자 입력과 간단한 조건문
+# Day 2: Getting user input and using conditional statements
 
-# 사용자에게 이름을 입력받는다
-name = input("당신의 이름은 무엇인가요? ")
-print("안녕하세요, " + name + "님!")
+# Ask for user's name
+name = input("What’s your name? ")
+print("Hi there, " + name + "!")
 
-# 나이를 입력받아서 정수형으로 변환
-age = int(input("몇 살이신가요? "))
+# Ask for age and convert to integer
+age = int(input("How old are you? "))
 if age >= 20:
-    print("성인이시네요!")
+    print("You're an adult!")
 else:
-    print("아직 미성년자시네요!")
+    print("You're still a minor!")
 
-# 숫자를 입력받고 홀수인지 짝수인지 판단
-num = int(input("숫자를 입력해주세요: "))
+# Ask for a number and check if it's even or odd
+num = int(input("Enter a number: "))
 if num % 2 == 0:
-    print("짝수입니다.")
+    print("That's an even number.")
 else:
-    print("홀수입니다.")
+    print("That's an odd number.")
